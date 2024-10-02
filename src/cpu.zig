9e55e30946d35e2df5da39d3b441b414cc9cdb46
Connor Garey <connorgarey1999@gmail.com>
2024-10-02 14:32:36 +0100

    Added R for register

--- src/cpu.zig
+++ src/cpu.zig
@@ -3,10 +3,10 @@
 
 const CPU = struct {
     // Registers 
-    A: u8, // Accumulator register
-    P: u8, // Status register - Only 7 bits are used, bit 5 is always 1
-    PC: u16, // Program counter
-    S: u8, // Stack Pointer 
-    X: u8, // index register
-    Y: u8, // index register
+    RA: u8, // Accumulator register
+    RP: u8, // Status register - Only 7 bits are used, bit 5 is always 1
+    RPC: u16, // Program counter
+    RS: u8, // Stack Pointer 
+    RX: u8, // index register
+    RY: u8, // index register
 };
