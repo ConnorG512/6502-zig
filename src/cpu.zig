0fb4dc8d31899330ade80899852d797424c17bf8
Connor Garey <connorgarey1999@gmail.com>
2024-10-02 17:36:52 +0100

    Changed to assignInstruction

--- src/cpu.zig
+++ src/cpu.zig
@@ -4,18 +4,33 @@ const std = @import("std");
 // http://www.6502.org/users/obelisk/6502/instructions.html
 // https://www.ahl27.com/posts/2023/01/6502-emu1/
 // https://archive.org/details/mos_microcomputers_programming_manual
+// http://www.6502.org/tutorials/65c02opcodes.html
+
+const CPUError = error {
+    nullByte,
+};
 
 const CPU = struct {
     // Registers 
     RA: u8, // Accumulator register
     RP: u8, // Status register - Only 7 bits are used, bit 5 is always 1
-    RPC: u16, // Program counter
     RS: u8, // Stack Pointer 
     RX: u8, // index register
     RY: u8, // index register
+    RPC: u16, // Program counter
 
-    pub fn readMemory() !void {
+    pub fn assignInstruction(self: CPU, byteToRead: *u8) !void {
 
+        if (byteToRead == null) { // Error handling
+            std.debug.print("Error, Byte is null!", .{});
+            return CPUError.nullByte;
+        }   
+        
+        switch (byteToRead) {
+            // 6502 instruction set
+            0x
+        }
+        
     }
 
 };
