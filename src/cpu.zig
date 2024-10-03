84b052927002d1eb4c2931a34beb09fafe023653
Connor Garey <connorgarey1999@gmail.com>
2024-10-03 17:20:34 +0100

    added error prints to flag functions

--- src/cpu.zig
+++ src/cpu.zig
@@ -110,6 +110,7 @@ const CPU = struct {
     // flag 5 is unused in 6502 and will always be set to the value of 1
     fn setAllFlags(self: *CPU) !void {
         if (self == null) {
+            std.debug.print("Error: CPU null reference in setAllFlags() !", .{});
             return CPUError.null_cpu_ref;
         }
         
@@ -118,10 +119,12 @@ const CPU = struct {
 
     fn setFlag(self: *CPU, flag:u8) !void {
         if (self == null) {
+            std.debug.print("Error: CPU null reference in setFlag() !", .{});
             return CPUError.null_cpu_ref;
         }
 
         if (flag > 0b1_1_1_1_1_1_1_1) {
+            std.debug.print("Error: Flag overflow in setFlag() !", .{});
             return CPUError.cpu_flag_overflow;
         }
 
@@ -131,6 +134,7 @@ const CPU = struct {
     
     fn clearAllFlags (self: *CPU) !void {
         if (self == null) {
+            std.debug.print("Error: CPU null reference in clearAllFlags() !", .{});
             return CPUError.null_cpu_ref;
         }
 
@@ -139,13 +143,15 @@ const CPU = struct {
 
     fn clearFlag(self: *CPU, flag:u8) !void {
         if (self == null) {
+            std.debug.print("Error: CPU null reference in clearFlag() !", .{});
             return CPUError.null_cpu_ref;
         }
         
         if (flag > 0b1_1_1_1_1_1_1_1) {
+            std.debug.print("Error: Flag overflow in clearFlag() !", .{});
             return CPUError.cpu_flag_overflow;
         }
-        
+
         // bitwise and (&) not (~) operation to set the respective bits to 0
         self.RP &= ~flag;
     }
