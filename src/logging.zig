6f6d393244bf9a93e6c4cd15bf95683f888a1c9d
Connor Garey <connorgarey1999@gmail.com>
2024-10-04 01:01:01 +0100

    Removed the logging from a struct

--- src/logging.zig
+++ src/logging.zig
@@ -1,18 +1,17 @@
 const std = @import("std");
 
-const logging = struct {
-    // ANSI escape codes for colours
-    const reset = "\x1b[0m"; // Reset to default color
-    const red = "\x1b[31m";   // Red text
-    const yellow = "\x1b[33m";   // Yellow text
-    
-    pub fn infoLog(message: []const u8) void {
-        std.debug.print("ERROR: {}", .{message});
-    }
-    pub fn errorLog(message: []const u8) void {
-        std.debug.print("{}ERROR: {}", .{red, message});
-    }
-    pub fn warnLog(message: []const u8) void {
-        std.debug.print("{}ERROR: {}", .{yellow, message});
-    }
-};
+
+ // ANSI escape codes for colours
+const reset = "\x1b[0m"; // Reset to default color
+const red = "\x1b[31m";   // Red text
+const yellow = "\x1b[33m";   // Yellow text
+   
+pub fn infoLog(message: []const u8) void {
+    std.debug.print("ERROR: {}", .{message});
+}
+pub fn errorLog(message: []const u8) void {
+    std.debug.print("{}ERROR: {}", .{red, message});
+}
+pub fn warnLog(message: []const u8) void {
+    std.debug.print("{}ERROR: {}", .{yellow, message});
+}
