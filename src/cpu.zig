736c65daef7db175b08001937c0bf7f202451bba
Connor Garey <connorgarey1999@gmail.com>
2024-10-09 16:21:00 +0100

    moved flag code to cpu_flag

--- src/cpu.zig
+++ src/cpu.zig
@@ -176,56 +176,6 @@ const CPU = struct {
 
     }
 
-    // Set flags for functions
-    // flag 5 is unused in 6502 and will always be set to the value of 1
-    fn setAllFlags(self: *CPU) !void {
-        if (self == null) {
-            logging.errorLog("Error: CPU null reference in setAllFlags() !");
-            return CPUError.null_cpu_ref;
-        }
-        
-        self.RP = 0b1_1_1_1_1_1_1_1;
-    }
-
-    fn setFlag(self: *CPU) !void {
-
-        if (self == null) {
-            logging.errorLog("Error: CPU null reference in setFlag() !");
-            return CPUError.null_cpu_ref;
-        }
-
-        switch (self.cpuFlag) {
-            cpuflag.carry_f => {
-
-            } 
-
-        }
-    }
-    
-    fn clearAllFlags (self: *CPU) !void {
-        if (self == null) {
-            logging.errorLog("Error: CPU null reference in clearAllFlags() !");
-            return CPUError.null_cpu_ref;
-        }
-
-        self.RP = 0b0_0_0_0_1_0_0_0;
-    }
-
-    fn clearFlag(self: *CPU, flag:u8) !void {
-        if (self == null) {
-            logging.errorLog("Error: CPU null reference in clearFlag() !");
-            return CPUError.null_cpu_ref;
-        }
-        
-        if (flag > 0b1_1_1_1_1_1_1_1) {
-            logging.errorLog("Error: Flag overflow in clearFlag() !");
-            return CPUError.cpu_flag_overflow;
-        }
-
-        // bitwise and (&) not (~) operation to set the respective bits to 0
-        self.RP &= ~flag;
-    }
-
     /////////////////////////////////////////////////////////////////////////
     // ADDRESSING MODES
     /////////////////////////////////////////////////////////////////////////
