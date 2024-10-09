c940eb457795a164d6fc300a6907028b449e25e4
Connor Garey <connorgarey1999@gmail.com>
2024-10-09 16:22:52 +0100

    Added license

--- src/cpu_flag.zig
+++ src/cpu_flag.zig
@@ -1,3 +1,21 @@
+// 6502-zig, emulation of the 6502 microprocessor in Zig.
+//Copyright (C) <2024>  <Connor Garey>
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const logging = @import("logging.zig");
+
 const cpu_flag = struct {
     const cpuFlag = enum {
         carry_f,
@@ -12,7 +30,7 @@ const cpu_flag = struct {
 
     // Set flags for functions
     // flag 5 is unused in 6502 and will always be set to the value of 1
-    fn setAllFlags(self: *CPU) !void {
+    fn setAllFlags() !void {
         if (self == null) {
             logging.errorLog("Error: CPU null reference in setAllFlags() !");
             return CPUError.null_cpu_ref;
