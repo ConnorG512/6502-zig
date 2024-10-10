7d69d0be2caa78406e341d51738ed29c637a0645
Connor Garey <connorgarey1999@gmail.com>
2024-10-10 16:37:21 +0100

    register transfers

--- src/cpu_instructions.zig
+++ src/cpu_instructions.zig
@@ -14,16 +14,58 @@
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+const logging = @import("logging.zig");
+
 pub const CPU_Instruction = struct {
 
     ///////////////////////////////////////
     // Load / Store Operations
     ///////////////////////////////////////
     
+    pub fn LDA() void {
+        logging.infoLog("cpu_instructions: LDA Called!");
+    }
+
+    pub fn LDX() void {
+        logging.infoLog("cpu_instructions: LDX Called!");
+    }
+
+    pub fn LDY() void {
+        logging.infoLog("cpu_instructions: LDY Called!");
+    }
+
+    pub fn STA() void {
+        logging.infoLog("cpu_instructions: STA Called!");
+    }
+
+    pub fn STX() void {
+        logging.infoLog("cpu_instructions: STX Called!");
+    }
+
+    pub fn STY() void {
+        logging.infoLog("cpu_instructions: STY Called!");
+    }
+
     ///////////////////////////////////////
     // Register transfers
     ///////////////////////////////////////
+    
+    pub fn TAX() {
+        logging.infoLog("cpu_instructions: TAX Called!");
+    }
 
+    pub fn TAY() {
+        logging.infoLog("cpu_instructions: TAY Called!");
+    }
+
+    pub fn TXA() {
+        logging.infoLog("cpu_instructions: TXA Called!");
+    }
+
+    pub fn TYA() {
+        logging.infoLog("cpu_instructions: TYA Called!");
+    }
+    
     ///////////////////////////////////////
     // Stack Operations
     ///////////////////////////////////////
