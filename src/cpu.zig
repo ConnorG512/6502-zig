700b6e7b7423dc207bfac730fefbd4f348fc1ffc
Connor Garey <connorgarey1999@gmail.com>
2024-10-10 22:44:47 +0100

    changed to array length to 255

--- src/cpu.zig
+++ src/cpu.zig
@@ -49,7 +49,7 @@ pub const CPU = struct {
 
     // Function to use in the array instruction index
     pub const instructionFn = fn(Self: *CPU, memory: *const [65536]u8, addressing_mode: *cpu_instruction_module.addressingMode) void;
-    pub const INSTRUCTION_SET = [_]instructionFn {
+    pub const INSTRUCTION_SET = [255]instructionFn {
         cpu_instruction_module.LDA,
     };
 
